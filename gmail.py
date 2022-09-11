ae857eeda0b603828a50af49bf5d780beac12508
david alvarez <david@southfloridaathleticclub.com>
2022-09-11 17:43:39 -0400

    COMPLETED

--- gmail.py
+++ gmail.py
@@ -16,9 +16,7 @@ def send_mail():
         message = EmailMessage()
 
         message.set_content(f'''
-        
-        'The sheet has been updated on ' + {datetime.today().strftime('%d/%m/%Y')}
-        \n
+        The sheet has been updated on {datetime.today().strftime('%d/%m/%Y')}
         \n
         Click the linkerino: {str(os.environ.get('SPREADSHEET_LINK'))}
         ''')
