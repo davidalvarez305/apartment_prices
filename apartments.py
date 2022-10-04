d5dc6e97e47807b865901eaed597895b7dc1f8e9
david alvarez <david@southfloridaathleticclub.com>
2022-10-03 22:07:48 -0400

    CLEAN CODE CODY

--- apartments.py
+++ apartments.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import os
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -7,9 +8,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from sheets import get_values
 
+def get_apt_details(container, selector):
+    text = container.find_element(
+                    By.CSS_SELECTOR, selector).get_attribute('innerText').strip().split('\n')[1]
+    return text
 
-def get_prices(url, range, sheet_id):
-
+def get_prices():
+    load_dotenv()
     options = Options()
     user_agent = str(os.environ.get('USER_AGENT'))
     options.add_argument("--headless")
@@ -18,58 +23,52 @@ def get_prices(url, range, sheet_id):
     driver = webdriver.Chrome(service=Service(
         ChromeDriverManager().install()), options=options)
 
-    driver.get(url)
+    driver.get(
+        'https://www.apartments.com/district-west-gables-west-miami-fl/3f1qyse/')
 
     apts = []
-    grids = driver.find_elements(By.CLASS_NAME, "pricingGridItem")
+    grids = driver.find_elements(By.CLASS_NAME, "hasUnitGrid")
 
     for grid in grids:
+
+        # Get Grid Details
+        details = grid.find_element(
+            By.CLASS_NAME, "detailsTextWrapper")
+        tags = details.find_elements(By.TAG_NAME, "span")
+
+        beds = tags[0].get_attribute('innerText')
+
+        grid_beds = ""
+        grid_baths = ""
+
+        if "Studio" in beds:
+            grid_beds = '0.5'
+        else:
+            grid_beds = beds.split('bed')[0]
+
+        grid_baths = tags[1].get_attribute(
+            'innerText').split('bath')[0]
+
         containers = grid.find_elements(By.CLASS_NAME, "grid-container")
         for container in containers:
             try:
-
                 unit = {}
 
                 # Get Unit Name
-                unit_selector = container.find_element(
-                    By.CLASS_NAME, "unitColumn").get_attribute('innerHTML').split('\n')[2]
-                splitter = '<span class="screenReaderOnly">Unit</span>'
-                if splitter in unit_selector:
-                    unit['name'] = unit_selector.split(splitter)[1].strip()
-
+                unit['name'] = get_apt_details(container, 'button.unitBtn')
+                
                 # Get Unit Price
-                price_selector = container.find_element(
-                    By.CLASS_NAME, "pricingColumn")
-                tags = price_selector.find_elements(By.TAG_NAME, "span")
-                if len(tags) > 0:
-                    unit['price'] = tags[1].get_attribute('innerHTML').strip()
+                unit['price'] = get_apt_details(container, "div.pricingColumn")
 
                 # Get Sqt. Ft
-                size_selector = container.find_element(By.CLASS_NAME, "sqftColumn")
-                tags = size_selector.find_elements(By.TAG_NAME, "span")
-                if len(tags) > 0:
-                    unit['size'] = tags[1].get_attribute('innerHTML').strip()
+                unit['size'] = get_apt_details(container, "div.sqftColumn")
 
                 # Get Availability
-                availability_selector = container.find_element(
-                    By.CLASS_NAME, "availableColumn")
-                tags = availability_selector.find_elements(By.TAG_NAME, "span")
-                if len(tags) > 0:
-                    unit['availability'] = tags[0].get_attribute('innerHTML').split(
-                        '<span class="screenReaderOnly">availibility </span>')[1].strip().strip()
+                unit['availability'] = get_apt_details(container, "div.availableColumnInnerContainer")
 
-                # Get Apt Details
-                details = grid.find_element(By.CLASS_NAME, "detailsTextWrapper")
-                tags = details.find_elements(By.TAG_NAME, "span")
-                beds = tags[0].get_attribute('innerHTML')
-                if "Studio" in beds:
-                    unit['beds'] = 0.5
-                else:
-                    unit['beds'] = tags[0].get_attribute(
-                        'innerHTML').split('bed')[0].strip()
-
-                unit['baths'] = tags[1].get_attribute(
-                    'innerHTML').split('bath')[0].strip()
+                # Get Bed & Baths
+                unit['beds'] = grid_beds
+                unit['baths'] = grid_baths
 
                 if "price" in unit:
                     converted_price = float(
@@ -80,32 +79,43 @@ def get_prices(url, range, sheet_id):
                 else:
                     unit['priceSqFt'] = '0'
 
+                # Concessions
+                concessions = driver.find_elements(
+                    By.ID, 'rentSpecialsSection')
+
+                if len(concessions) > 0:
+                    unit['concessions'] = concessions[0].find_element(
+                        By.TAG_NAME, 'p').get_attribute('innerText')
+
                 if "name" in unit:
                     apts.append(unit)
 
-            except BaseException as err:
-                print(err)
+            except BaseException:
                 continue
 
-    try:
-        rows = get_values(spreadsheet_id=sheet_id, range=range)
 
-        apartments = []
-        for apt in apts:
-            vals = [''] * len(rows[0])
-            vals[0] = datetime.today().strftime('%m/%d/%Y')
-            vals[1] = apt['name']
-            vals[2] = apt['beds']
-            vals[3] = apt['baths']
-            vals[4] = apt['size']
-            vals[5] = apt['price']
-            vals[6] = apt['priceSqFt']
-            vals[7] = apt['availability']
+"""  try:
+    rows = get_values(spreadsheet_id=sheet_id, range=range)
 
-            apartments.append(vals)
-        rows += apartments
+    apartments = []
+    for apt in apts:
+        vals = [''] * len(rows[0])
+        vals[0] = datetime.today().strftime('%m/%d/%Y')
+        vals[1] = apt['name']
+        vals[2] = apt['beds']
+        vals[3] = apt['baths']
+        vals[4] = apt['size']
+        vals[5] = apt['price']
+        vals[6] = apt['priceSqFt']
+        vals[7] = apt['availability']
+        if 'concessions' in apt:
+            vals[8] = apt['concessions']
 
-        return rows
+        apartments.append(vals)
+    rows += apartments
 
-    except BaseException as err:
-        raise Exception(err)
+    return rows
+
+except BaseException as err:
+    raise Exception(err) """
+get_prices()
